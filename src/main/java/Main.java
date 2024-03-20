166d981f800254b8ab101ec48b90e8720f90e5a7
talgabay7 <126403499+talgabay7@users.noreply.github.com>
2024-03-20 20:08:43 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -52,6 +52,69 @@ class Main {
 		ranges.setNext(new Node<>(range));
 		return first.getNext();
 	}
+	public static Scanner input=new Scanner(System.in);
+	public static int length(Node<TavPlace> p) {
+		int count=0;
+		while(p!=null) {
+			count++;
+			p=p.getNext();
+		}
+		return count;
+	}
+	public static Node<String> word(Node<TavPlace> p){
+		int len=length(p);
+		Node<TavPlace> firstT=p;
+		Node<String> letters=new Node<>(null);
+		Node<String> firstL=letters;
+		for(int i=1;i<=len;i++) {
+			while(p.getValue().getPlace()!=i)
+				p=p.getNext();
+			letters.setNext(new Node<String>(p.getValue().getTav()));
+			letters=letters.getNext();
+			p=firstT;
+		}
+		return firstL.getNext();
+	}
+	public static String makeSentence(Node<TavPlace>[] a) {
+		String sentence="";
+		Node<String> word;
+		for(int i=0;i<a.length;i++) {
+			word=word(a[i]);
+			while(word!=null) {
+				sentence+=word.getValue();
+				word=word.getNext();
+			}
+			sentence+=" ";
+		}
+		return sentence;
+	}
+	public static int dis(Queue<Integer> q,int x) {
+		boolean flag=false;
+		int count=-1;
+		int distance=0;
+		q.insert(null);
+		while(q.head()!=null) {
+			if(flag)
+				count++;
+			if(q.head()==x)
+				flag=!flag;
+			q.insert(q.remove());
+		}
+		q.remove();
+		return distance;
+	}
+	public static int minDis(Queue<Integer> q) {
+		int min=dis(q,q.head());
+		Queue<Integer> q2=new Queue<>();
+		while(!q.isEmpty()) {
+			if(min>dis(q,q.head()))
+				min=dis(q,q.head());
+			q2.insert(q.remove());
+		}
+		while(!q2.isEmpty())
+			q.insert(q2.remove());
+		return min;
+	}
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
