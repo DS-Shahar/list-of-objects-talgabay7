60658585aa70ad3849c2a5df0a886c0241df8d0f
talgabay7 <126403499+talgabay7@users.noreply.github.com>
2024-03-20 20:08:03 +0200

    Create TavPlace.java

---
+++ src/main/java/TavPlace.java
@@ -0,0 +1,21 @@
+public class TavPlace {
+	private String tav;
+	private int place;
+	public TavPlace(String tav, int place) {
+		this.tav = tav;
+		this.place = place;
+	}
+	public String getTav() {
+		return tav;
+	}
+	public void setTav(String tav) {
+		this.tav = tav;
+	}
+	public int getPlace() {
+		return place;
+	}
+	public void setPlace(int place) {
+		this.place = place;
+	}
+	
+}
